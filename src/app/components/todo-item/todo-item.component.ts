9b924fd9c17c7a1ebb2c3268381f8b7e34fd12ab
QuyenTran1002 <btran1002@gatech.edu>
2019-05-13 00:30:04 -0400

    updated toggle menu

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import {TodoService} from '../../services/todo.service';
 import { Todo } from 'src/app/models/Todo';
 
 // @ts-ignore
@@ -9,7 +10,7 @@ import { Todo } from 'src/app/models/Todo';
 })
 export class TodoItemComponent implements OnInit {
   @Input() todo: Todo;
-  constructor() { }
+  constructor(private todoService: TodoService) { }
 
   ngOnInit() {
   }
@@ -23,8 +24,11 @@ export class TodoItemComponent implements OnInit {
   }
 
   onToggle(todo) {
+    // Toggle in UI
     // console.log('toggle');
     todo.completed = !todo.completed;
+    // Toggle onserver
+    this.todoService.toggleCompleted(todo).subscribe(todo => console.log(todo));
   }
 
   onDelete(todo) {
--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -4,6 +4,11 @@ import { HttpClient, HttpHeaders} from '@angular/common/http';
 
 import { Todo} from '../models/Todo';
 // import {Observable} from 'rxjs';
+const httpOptions = {
+  headers: new HttpHeaders( {
+    'Content-Type': 'application/json'
+  })
+}
 
 @Injectable({
   providedIn: 'root'
@@ -11,9 +16,16 @@ import { Todo} from '../models/Todo';
 export class TodoService {
   todosUrl: string = 'https://jsonplaceholder.typicode.com/todos';
   todosLimit = '?_limit=5';
-  constructor(private http:HttpClient) { }
+  constructor(private http: HttpClient) { }
 
+  // Get todos
   getTodos(): Observable<Todo[]> {
     return this.http.get<Todo[]>(`${this.todosUrl}${this.todosLimit}`);
+  }
+
+  // Toggle Completed
+  toggleCompleted(todo: Todo): Observable<any> {
+    const url = `${this.todosUrl}/${todo.id}`;
+    return this.http.put(url, todo, httpOptions);
   }
 }
